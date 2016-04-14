1b4dbfb41acb691a58e5c30b81cfa802628305b2
rysmith18 <rtsmith1101@outlook.com>
2016-04-14 10:30:01 -0400

    MyLine moves!!
    Only thing left is MyStar!!  Thanks!!

--- src/MyLine.java
+++ src/MyLine.java
@@ -18,6 +18,7 @@ public class MyLine implements DrawingObject{
 	
 	int sizeX, sizeY, originX, originY;
 	int lastX, lastY;
+	
 	Rectangle bounds = new Rectangle();
 	public MyLine() {
 		sizeX = sizeY = originX = originY = 0;
@@ -25,6 +26,16 @@ public class MyLine implements DrawingObject{
 		
 		System.out.println("Made line @" + originX + ", " + originY + "; " + sizeX + " x " + sizeY);
 	}
+	
+	public MyLine( int oX, int oY, int sX, int sY ) {
+        sizeX = sX;
+        sizeY = sY;
+        originX = oX;
+        originY = oY;
+        setBounds( bounds );
+        
+        
+    }
 
 	@Override
 	public void draw(Graphics g) {
@@ -55,8 +66,13 @@ public class MyLine implements DrawingObject{
 
 	@Override
 	public void move(Point p) {
+		int xLngth = sizeX - originX;
+		int yLngth = sizeY - originY;
+		
 		originX = p.x;
         originY = p.y;
+        sizeX = originX + xLngth;
+        sizeY = originY + yLngth;
         setBounds( bounds );
 		
 	}
@@ -68,17 +84,25 @@ public class MyLine implements DrawingObject{
 		
 	}
 	
-	public int distance(Point a, Point b) {
-		Math.sqrt(((a.x - b.x) * (a.x - b.x)) - ((a.y - b.y) * (a.y - b.y)));
+	public double distance(Point a, Point b) {
+		int in = 0;
+		in = (int)Math.sqrt(((a.x - b.x) * (a.x - b.x)) + ((a.y - b.y) * (a.y - b.y)));
 		
-		return lastX;
+		return in;
 	}
 
 	@Override
 	public boolean contains(Point p) {
 		// TODO Auto-generated method stub
-		
-		return bounds.contains(p);
+		Point a = new Point(originX, originY);
+		Point b = new Point(sizeX, sizeY);
+		System.out.println(distance(a, p) + distance(p, b));
+		System.out.println(distance(a, b));
+		if(distance(a, p) + distance(p, b) >= distance(a, b) - 1 && distance(a, p) + distance(p, b) <= distance(a, b) + 1) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 
